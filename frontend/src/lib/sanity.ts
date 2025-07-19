4be687dd3d319d5ccfe096ad2d2ae4a1b87d7aff
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2025-07-20 01:27:14 +0500

    add

--- frontend/src/lib/sanity.ts
+++ frontend/src/lib/sanity.ts
@@ -2,7 +2,7 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2023-07-18",
   useCdn: false,
